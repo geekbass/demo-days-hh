79faa5229e0e8493995e2a219c43487efb666859
Weston Bassler <wbassler@mesosphere.com>
2018-10-16 11:04:13 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+}
+
+provider "google" {
+}
+
+variable "dcos_install_mode" {
+  description = "specifies which type of command to execute. Options: install or upgrade"
+  default     = "install"
+}
+
+data "http" "whatismyip" {
+  url = "http://whatismyip.akamai.com/"
+}
+
+module "dcos" {
+  source = "dcos-terraform/dcos/aws"
+
+  dcos_instance_os    = "centos_7.3"
+  cluster_name        = "dcos-demo"
+  ssh_public_key_file = "~/.ssh/id_rsa.pub"
+  admin_ips           = ["${data.http.whatismyip.body}/32"]
+
+  num_masters        = "1"
+  num_private_agents = "2"
+  num_public_agents  = "1"
+
+  dcos_version = "1.11.4"
+
+  dcos_variant = "open"
+  # dcos_variant              = "ee"
+  # dcos_license_key_contents = "${file("./license.txt")}"
+
+  dcos_install_mode = "${var.dcos_install_mode}"
+}
+
+output "masters-ips" {
+  value = "${module.dcos.masters-ips}"
+}
+
+output "cluster-address" {
+  value = "${module.dcos.masters-loadbalancer}"
+}
+
+output "public-agents-loadbalancer" {
+  value = "${module.dcos.public-agents-loadbalancer}"
+}
